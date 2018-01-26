0ea87243b8ad9a5c81f21ad04a02fa76eec8de52
VITOR BARBOSA <vitorbarbosa19@gmail.com>
2018-01-25 17:29:43 -0800

    first commit. Webpack and babel config

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+const path = require('path')
+
+const config = {
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'build')
+		filename: 'index.bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: [
+							'@babel/preset-env',
+							'@babel/preset-react'
+						],
+						plugins: [
+							'@babel/transform-runtime'
+							'@babel/plugin-proposal-class-properties'
+						]
+					}
+				}
+			},
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'raw-loader']
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({ template: './src/index.html' }),
+	]
+}
+
+if (process.env.NODE_ENV === 'production') {
+	config.plugins.push(
+		new webpack.optimize.UglifyJsPlugin(),
+		new webpack.optimize.ModuleConcatenationPlugin(),
+		new webpack.DefinePlugin({
+			'process.env': {
+				NODE_ENV: JSON.stringify('production')
+			}
+		})
+	)
+}
+
+module.exports = config
