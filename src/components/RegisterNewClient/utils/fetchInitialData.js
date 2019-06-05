70464453b39633002e0b4dc6f6f748d761f64cef
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-06-05 06:25:31 -0700

    add new option to hunters list

--- src/components/RegisterNewClient/utils/fetchInitialData.js
+++ src/components/RegisterNewClient/utils/fetchInitialData.js
@@ -5,7 +5,7 @@ const fetchInitialData = async () => {
 		const { data: { values: values_rep } } = await get(process.env.REPRESENTATIVES)
 		const { data: { values: values_hun } } = await get(process.env.HUNTERS)
 		const representatives = values_rep.map( value => value[0] ).slice(1).sort()
-		const hunters = values_hun.map( value => value[0] ).slice(1).sort()
+		const hunters = ['Nenhum', ...values_hun.map( value => value[0] ).slice(1).sort()]
 		return { representatives, hunters }
 	} catch (error) {
 		console.log(error)
